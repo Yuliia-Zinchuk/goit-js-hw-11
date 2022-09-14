ed0374672cad40e4cd32f31f18e8a433eb713be7
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-14 14:58:15 +0300

    update

--- src/galleryServiceAPI.js
+++ src/galleryServiceAPI.js
@@ -1,60 +1,32 @@
-// key - твой уникальный ключ доступа к API.
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-//     safesearch - фильтр по возрасту.Задай значение true.
-
 import Notiflix from 'notiflix';
 import axios from 'axios';
 const API_KEY = '29884579-b0e414ddacb31e478cf055115';
 const BASE_URL = 'https://pixabay.com/api';
 const fieldParams = 'image_type=photo&orientation=horizontal&safesearch=true';
-const per_page = 40;
-//import loadMoreButton from './index';
-//console.log(loadMoreButton);
+export const per_page = 40;
+
 import { invisibleButton } from './index';
-console.log(invisibleButton);
+
 let tothits = 0;
 const axios = require('axios');
-// const options = {
-//     headers: {
-//         Authorization: API_KEY ,
-//     },
 
-export default class GalleryApiService {
+export class GalleryApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
     // this.per_page = 5;
   }
 
-  //   async fetchPictures() {
-  //     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&${fieldParams}&per_page=${per_page}&page=${this.page}`;
-
-  //     const response = await fetch(url);
-
-  //     console.log(response);
-
-  //     const hits = await response.json();
-  //     console.log(hits);
-
   async fetchPictures() {
     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&${fieldParams}&per_page=${per_page}&page=${this.page}`;
-
     const response = await axios.get(url);
-
-    // console.log(response);
     const hits = response.data;
-    // response = await (response => ;
-    console.log(hits);
 
     tothits = hits.totalHits;
-    if (tothits === 0) {
+    if (hits === []) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-
-      //alert('rrr');
     }
     const totalPages = tothits / per_page;
     console.log(totalPages);
@@ -66,45 +38,13 @@ export default class GalleryApiService {
     if (this.page > Math.ceil(totalPages)) {
       invisibleButton();
       Notiflix.Notify.info(
-        'Sorry, there are no images matching your search query. Please try again.'
+        "We're sorry, but you've reached the end of search results."
       );
-      invisibleButton();
+      // invisibleButton();
     }
-    // console.log('gjckt pfghjcf', tothits);
 
     return hits.hits;
   }
-
-  // console.log(tothits);
-  // console.log(hits);
-
-  // const hits1 = ({ hits }) => {
-  //   //       //         //console.log(data);
-  //   //       this.incrementPage();
-  //   return hits;
-  // };
-  // console.log(hits1);
-
-  // this.incrementPage();
-  // }
-
-  //     console.log();
-
-  //     return
-  //       .then(response => response.json())
-  //       .then(({ hits }) => {
-  //         //console.log(data);
-  //         this.incrementPage();
-  //         // loadMoreButton.setAttribute('hidden', false);
-  //         // console.log('после запроса', this);
-  //         //loadMoreButton.setAttribute('hidden', true);
-
-  //         return hits;
-  //       })
-  //       .catch(error => {
-  //         console.log('oops');
-  //       });
-  //   }
 
   incrementPage() {
     this.page += 1;
@@ -121,18 +61,3 @@ export default class GalleryApiService {
     this.searchQuery = newQuery;
   }
 }
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => {
-    console.log(response);
-  })
-  .then(response => {
-    console.log(response);
-  })
-  .then(body => {
-    console.log(body);
-    console.log(data);
-  })
-  .catch(error => {
-    // Error handling
-  });
--- src/index.js
+++ src/index.js
@@ -1,77 +1,49 @@
 import SimpleLightbox from 'simplelightbox';
-import Notiflix from 'notiflix';
-import GalleryApiService from './galleryServiceAPI';
-
-// Описан в документации
-//import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import Notiflix from 'notiflix';
+import { GalleryApiService } from './galleryServiceAPI';
+import { per_page } from './galleryServiceAPI';
 const searchForm = document.querySelector('.search-form');
+const searchBox = document.getElementsByName('searchQuery');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 
-//loadMoreButton.setAttribute('hidden', true);
-
-//loadMoreButton.removeAttribute('hidden', true);
 export function invisibleButton() {
   return loadMoreButton.setAttribute('hidden', true);
 }
 invisibleButton();
-//style="display: none;
+
 const galleryApiService = new GalleryApiService();
-console.log(galleryApiService);
 
 searchForm.addEventListener('submit', onSearch);
 loadMoreButton.addEventListener('click', onLoadMore);
 
-// btnStop.setAttribute('disabled', true);
-
 function onSearch(evt) {
   evt.preventDefault();
-  loadMoreButton.setAttribute('hidden', true);
+  if (searchBox.value === '') {
+    Notiflix.Notify.failure('Please, input your query.');
+    return;
+  }
+  invisibleButton();
+  //loadMoreButton.setAttribute('hidden', true);
   galleryApiService.searchQuery = evt.currentTarget.elements.searchQuery.value;
 
-  //   if (galleryApiService.searchQuery === '') {
-  //     return Notiflix.Notify.failure('Oops, there is no country with that names');
-  //   }
-
   galleryApiService.resetPage();
-  //galleryApiService.message1();
 
-  // loadMoreButton.setAttribute('hidden', true);
   galleryApiService.fetchPictures().then(hits => {
     clearGalleryContainer();
     renderPicturesList(hits);
-    if (hits.length >= 5) {
+    console.log(hits.length);
+    if (hits.length >= per_page) {
       loadMoreButton.removeAttribute('hidden');
     }
-    // message(hits);
-
-    // console.log(hits);
-    // if (hits === []) {
-    //   console.log(900);
-    //   //   return Notiflix.Notify.failure(
-    //   //     'Oops, there is no country with that names'
-    //   //   );
-    // }
   });
-
-  // .catch(error =>
-  //   Notiflix.Notify.failure('Oops, there is no country with that names')
-  // );
-
-  //   if (hits === []) {
-  //     return Notiflix.Notify.failure('Oops, there is no country with that names');
-  //   }
-  //loadMoreButton.removeAttribute('hidden');
 }
 
 function onLoadMore() {
   galleryApiService.fetchPictures().then(renderPicturesList);
-  // alert(`${tothits}`);
 }
-//console.log(galleryApiService.fetchPictures(tothits));
+
 function renderPicturesList(hits) {
   console.log(hits);
   const list = hits
@@ -110,31 +82,6 @@ function clearGalleryContainer() {
   galleryContainer.innerHTML = '';
 }
 
-// let gallery = `${'.photo-card div'}`.simpleLightbox();
-
 let gallery = new SimpleLightbox('.gallery a');
 
 gallery.refresh();
-// gallery.next(); // Next Image
-// new SimpleLightbox('.photo-card a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
-
-//  getNews(query) {
-//     if (query === undefined) query = this.config.data.q;
-//     else this.config.data.q = query;
-
-//     let serchParams = new URLSearchParams(this.config.data);
-//     return fetch(`${this.config.baseURL}?${serchParams}`, this.config)
-//       .then((resp) => resp.json())
-//       .then((data) => {
-//         this.totalPage = data.total_pages;
-//         return data;
-//       });
-//   }
-
-//   setPage(page) {
-//     this.config.data.page = page;
-//   }
-// }
